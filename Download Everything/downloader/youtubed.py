08e090e9eed74af12affdf89bc81d015e010abc3
shujinkou <merlierj@protonmail.com>
2020-10-15 21:37:23 -0400

    change yt downloader

--- Download Everything/downloader/youtubed.py
+++ Download Everything/downloader/youtubed.py
@@ -8,8 +8,8 @@ def download():
     video_url = url.get()
     try:
         youtube = pytube.YouTube(video_url)
-        video = youtube.streams.last()
-        video.download("C:\\Users\\Asus\\Downloads\\tester")
+        video = youtube.streams.first()
+        video.download("C:\\Users\Asus\Documents\projet-infini\Download Everything\downloader\output of youtubed")
         notif.config(fg="green", text="download complete")
     except Exception as e:
         print(e)
@@ -20,11 +20,17 @@ master.title("Video Downloader")
 
 Label(master, text="Youtube Video Converter", fg="red", font=("Calibri", 15)).grid(sticky=N,padx=100,row =0)
 Label(master, text="Please enter the link to your video below : ", font=("Calibri", 12)).grid(sticky=N,row =1,pady=15)
+
 notif = Label(master,font=("Calibri", 12))
+
 notif.grid(sticky=N,pady=1,row=4)
+
 url = StringVar()
 
+#enter textvariable url
 Entry(master,width=50,textvariable=url).grid(sticky=N,row=2)
+
+
 Button(master,width=20,text="Download",font=("Calibri",12), command=download).grid(sticky=N,row=3,pady=15)
 
 master.mainloop()
